8b28916917536874d64b8a65e0fbb3f120db5624
Frank Theile <ftheile@grundfos.com>
2016-10-07 15:21:06 +0200

    Remove quotes

--- home/cc-env.sh
+++ home/cc-env.sh
@@ -1,3 +1,3 @@
 export CROSS_COMPILE=arm-linux-
-export PATH="~/x-tools/arm-cortexa5-linux-uclibcgnueabihf/bin:$PATH"
+export PATH=~/x-tools/arm-cortexa5-linux-uclibcgnueabihf/bin:$PATH
 export ARCH=arm
