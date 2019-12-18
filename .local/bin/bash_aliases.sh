4cb7ddf99ea96d040479031b05932303e7af12f3
Frank Theile <ftheile@grundfos.com>
2019-12-18 11:23:41 +0100

    Add .local/bin/bash_aliases.sh

---
+++ .local/bin/bash_aliases.sh
@@ -0,0 +1,11 @@
+# All aliases for bash:
+alias ls='ls -A -F --color=auto'
+alias grep='grep --color'
+alias egrep='egrep --color'
+alias zgrep='zgrep --color'
+alias pactree='pactree --color'
+alias dmesg='dmesg --human'
+alias less='less --LINE-NUMBERS --ignore-case --LONG-PROMPT --window=-4 --tabs=4 --no-init --quit-if-one-screen --RAW-CONTROL-CHARS --shift .1'
+alias dd='dd status=progress'
+alias diff='diff --color'
+alias dotcfg='/usr/bin/git --git-dir=$HOME/.dotcfg --work-tree=$HOME'
