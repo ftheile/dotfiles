9ec264840c5fb475c116ec1460e38cb42362b96e
Frank Theile <frank.theile@web.de>
2023-02-05 15:08:04 +0100

    Move ~/.cargo to ~/.local/share/cargo
    See https://wiki.archlinux.org/title/XDG_Base_Directory#Supported

--- .local/bin/xdg_compat.sh
+++ .local/bin/xdg_compat.sh
@@ -3,3 +3,4 @@ export LESSKEY=~/.config/less/lesskey
 export LESSHISTFILE=~/.cache/less/history
 export INPUTRC=~/.config/readline/inputrc
 export HISTFILE=~/.cache/bash/history
+export CARGO_HOME=~/.local/share/cargo
