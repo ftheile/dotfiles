be42e6a1695374eec0fcfbfbdf00b848a5cf77bb
Frank Theile <ftheile@grundfos.com>
2023-02-06 07:48:17 +0100

    Move more dirs into XDG subfolders

--- .local/bin/xdg_compat.sh
+++ .local/bin/xdg_compat.sh
@@ -4,3 +4,5 @@ export LESSHISTFILE=~/.cache/less/history
 export INPUTRC=~/.config/readline/inputrc
 export HISTFILE=~/.cache/bash/history
 export CARGO_HOME=~/.local/share/cargo
+export PYLINTHOME=~/.cache/pylint
+export GRADLE_USER_HOME=~/.local/share/gradle
